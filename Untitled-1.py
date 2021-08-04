20e1c78592ba42f03c6e1b1a019b7e179e32697f
ivan <ivan.perez.alarcon@utem.cl>
2021-08-03 22:52:11 -0400

    Commit de prueba

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+print("Hello world")
+
+
+nums = [4, 78, 9, 84]
+for n in nums:
+    print(n)
+
+
+from datetime import date
+from datetime import datetime
+
+#Día actual
+today = date.today()
+
+#Fecha actual
+now = datetime.now()
+
+print(today)
+print(now)

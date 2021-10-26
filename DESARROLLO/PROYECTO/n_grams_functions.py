5d185f601038146dda14d7d923810e899b86bbba
ivan <ivan.perez.alarcon@utem.cl>
2021-10-26 01:07:18 -0300

    archivo de funciones para main n-grams

---
+++ DESARROLLO/PROYECTO/n_grams_functions.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import os
+from textblob import TextBlob
+import functions as f
+import bdd_functions as bdf
+
+def col_unique_values(col_string):
+    unicos = col_string.unique()
+    #print(unicos)
+    return unicos
+
+def col_bdd_unique_values(col_string,nom_col):
+    c3, cz = bdf.conectarse()
+    val_uni_col = []
+    cz.execute("""
+        SELECT "ID", "ID_DICC_DATOS", "VAL_UNICO"
+        FROM pruebas."UNIQUE_VALUES_STRING_COLUMNS"
+        WHERE "ID_DICC_DATOS" = (SELECT "ID" FROM pruebas."DICCIONARIO_DE_DATOS" WHERE "NOM_COL" = '{v1}');
+        """.format(v1 = nom_col))
+    for i in cz.fetchall():
+        #print(i[0],i[1],i[2])
+        val_uni_col.append(i[2])
+    #print(val_uni_col)
+    return val_uni_col
+
+def ngram(sentence, num):
+    tmp = [] 
+    sent_len = len(sentence) - num +1
+    for i in range(sent_len):
+        tmp.append(sentence[i:i+num]) 
+    return tmp
+
+def diff_ngram(sent_a, sent_b, num):
+    a = ngram(sent_a, num)
+    b = ngram(sent_b, num) 
+    common = [] 
+    cnt = 0 
+    for i in a:
+        for j in b:
+            if i == j:
+                cnt += 1
+                common.append(i)
+    return cnt/len(a), common

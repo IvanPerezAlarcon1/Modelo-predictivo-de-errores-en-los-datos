c701d37d27f269c420207688fa7892679a01f537
ivan <ivan.perez.alarcon@utem.cl>
2021-10-28 23:51:17 -0300

    Se temrinó el camino de una vez contextualizado el modelo, se ingresan nuevos datos, con problemas de valores que no se parecen a ninguno y valores parecidos a otros en columnas string

--- DESARROLLO/PROYECTO/n_grams_apply.py
+++ DESARROLLO/PROYECTO/n_grams_apply.py
@@ -5,45 +5,23 @@ import bdd_functions as bdf
 import n_grams_functions as ngf
 
 
-archivo = r'C:\Users\ivan1\Desktop\Modelo-predictivo-de-errores-en-los-datos\DESARROLLO\hotel_bookings_1_test2.csv' # se demora 2.8 seg en terminar
+#archivo = r'C:\Users\ivan1\Desktop\Modelo-predictivo-de-errores-en-los-datos\DESARROLLO\hotel_bookings_1_test2.csv' # se demora 2.8 seg en terminar
+archivo = r'C:\Users\ivan1\Desktop\Modelo-predictivo-de-errores-en-los-datos\DESARROLLO\hotel_bookings_1_NGRAMS-test-2-cols--witherrors.csv'
 #archivo = r'C:\Users\ivan1\Desktop\Modelo-predictivo-de-errores-en-los-datos\hotel_bookings_1.xlsx' # se demora 43.9 seg en terminar
 
 #------------------------MAIN------------------------------
 df = f.dataframe_from_file(archivo)
-#print(df)
-df_col_numericas,df_col_string = f.sep_col_string_and_num(df)
-
-#print(df_col_string.columns)
-for i in range(len(df_col_string.columns)):
-    #print(df_col_string[df_col_string.columns[i]])
-    unicos_col = ngf.col_unique_values(df_col_string[df_col_string.columns[i]])#df_col_string[df_col_string.columns[i]].unique()
-    aux = ngf.col_unique_values(df_col_string[df_col_string.columns[i]])
-    #print("VAL UNICOS ENTRADA: ", unicos_col)
-    unicos_col_bdd = ngf.col_bdd_unique_values(df_col_string[df_col_string.columns[i]],df_col_string.columns[i])
-    for j in aux:
-        if(j in unicos_col_bdd): #si un elemento del conjunto ingresado está en los unicos de la bdd, lo elimino ya que sería el escenario donde la similitud de los elementos seria igual y no requieren tratamiento 
-            unicos_col.remove(j)
-    print("VAL UNICOS ENTRADA no encontrados en BDD: ", unicos_col)
-    print("VALORES UNICOS BDD: ",unicos_col_bdd, '\n')
-
-    #se busca comparar el parecido de los valores nuevos, con relacion a los registros de la bdd, en base a las medidas indicadas para n-grams, se realizan las acciones
-    n_gram = 3
-    for l in unicos_col:
-        print("unicos_col: ",l)
-        for k in unicos_col_bdd:
-            print("unicos_col_bdd: ",k)
-            a1, w1 = ngf.diff_ngram(k,l,n_gram)
-            if(a1 >= 0.6):
-                print("3-grams - REEMPLAZA: ", a1, k, l)
-                df[df_col_string.columns[i]] = df[df_col_string.columns[i]].replace(to_replace = l,  value = k)
-                #df_col_string[df_col_string.columns[i]] = df_col_string[df_col_string.columns[i]].replace(to_replace = l,  value = k)
-                break
-            if(a1 < 0.6):
-                print("3-grams - NO REEMPLAZA, SE AGREGA A VALORES UNICOS: ", a1, k, l)
-        print('\n')
-    break
 
 print(df['hotel'].unique())
+print(df['deposit_type'].unique())
+
+df_col_numericas,df_col_string = f.sep_col_string_and_num(df)
+
+ngf.revisar_string_cols(df,df_col_string)
+
+print(df['hotel'].unique())
+print(df['deposit_type'].unique())
+
 #print(df_col_string['hotel'].unique())
 
 #a1, word1 = ngf.diff_ngram('Resort Htel','Resort Hotel', 3)
--- DESARROLLO/PROYECTO/n_grams_functions.py
+++ DESARROLLO/PROYECTO/n_grams_functions.py
@@ -17,14 +17,17 @@ def col_bdd_unique_values(col_string,nom_col):
     c3, cz = bdf.conectarse()
     val_uni_col = []
     cz.execute("""
-        SELECT "ID", "ID_DICC_DATOS", "VAL_UNICO"
-        FROM pruebas."UNIQUE_VALUES_STRING_COLUMNS"
-        WHERE "ID_DICC_DATOS" = (SELECT "ID" FROM pruebas."DICCIONARIO_DE_DATOS" WHERE "NOM_COL" = '{v1}');
+        SELECT u."ID", u."ID_DICC_DATOS", u."VAL_UNICO", d."N_GRAM"
+        FROM pruebas."DICCIONARIO_DE_DATOS" d
+        inner join pruebas."UNIQUE_VALUES_STRING_COLUMNS" u on u."ID_DICC_DATOS" = d."ID"
+        where d."NOM_COL" = '{v1}';
         """.format(v1 = nom_col))
     for i in cz.fetchall():
         #print(i[0],i[1],i[2])
         val_uni_col.append(i[2])
-    return val_uni_col
+        ID = i[1]
+        n_gram = i[3]
+    return val_uni_col, ID, n_gram
 
 def ngram(sentence, num):
     tmp = [] 
@@ -44,3 +47,56 @@ def diff_ngram(sent_a, sent_b, num):
                 cnt += 1
                 common.append(i)
     return cnt/len(a), common
+
+#def insert_unique_values_string(val_unicos, nom_col):
+#    c3, cz = conectarse()
+#    for i in len(val_unicos):
+#
+#    cz.close()
+
+def revisar_string_cols(df, df_string):
+    for i in range(len(df_string.columns)):
+        #valores unicos para la columna string analizada
+        unicos_col = col_unique_values(df_string[df_string.columns[i]])
+        aux = col_unique_values(df_string[df_string.columns[i]])
+        #valores unicos registrados en la bdd, para la columna string ingresada
+        unicos_col_bdd,ID, n_gram = col_bdd_unique_values(df_string[df_string.columns[i]], df_string.columns[i])
+        #si un elemento del conjunto ingresado está en los unicos de la bdd, lo elimino ya que sería el escenario donde la similitud de los elementos seria igual y no requieren tratamiento
+        for j in aux:
+            if(j in unicos_col_bdd):
+                unicos_col.remove(j)
+        print("VAL UNICOS ENTRADA no encontrados en BDD: ", unicos_col)
+        print("VALORES UNICOS BDD: ",unicos_col_bdd, '\n')
+
+        print(ID)
+        aux2 = 0
+        aux3 = 0
+        for l in unicos_col:
+            print("unicos_col: ", l)
+            for k in unicos_col_bdd:
+                print("unicos_col_bdd: ", k)
+                ind, com = diff_ngram(k,l, n_gram)
+                if(ind >= 0.6):
+                    print("3-grams - REEMPLAZA: ", ind, k, l)
+                    df[df_string.columns[i]] = df[df_string.columns[i]].replace(to_replace = l,  value = k)
+                    #df_string[df_string.columns[i]] = df_string[df_string.columns[i]].replace(to_replace = l,  value = k)
+                    aux2 = 1
+                    break
+                if(ind < 0.6):
+                    print("3-grams - NO REEMPLAZA, SE AGREGA A VALORES UNICOS: ", ind, k, l)
+                    #SE INSERTAN LOS VALORES UNICOS
+                    c3, cz = bdf.conectarse()
+                    cz.execute(""" insert into pruebas."UNIQUE_VALUES_STRING_COLUMNS"("ID_DICC_DATOS","VAL_UNICO") VALUES 
+                          ('{v1}','{v2}'); COMMIT;
+                        """.format(v1 = ID , v2 = l ))
+                    cz.close()
+                    aux2 = 1
+                    break
+                else:
+                    print("RESULTADO INDICE DE COMPARACIÓN INVÁLIDO: ", ind)
+                    aux3 = 1
+            print('\n')
+            if(aux == 1):
+                break
+        if(aux3 == 1):
+            break

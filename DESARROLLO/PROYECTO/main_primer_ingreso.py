00d0ffae80a000c5c506d59f720f66f24e64f40c
ivan <ivan.perez.alarcon@utem.cl>
2021-12-01 00:53:28 -0300

    Agrega mensajes de que tipo de correccion se está haciendo

--- DESARROLLO/PROYECTO/main_primer_ingreso.py
+++ DESARROLLO/PROYECTO/main_primer_ingreso.py
@@ -7,7 +7,7 @@ import imputaciones_functions as imf
 import n_grams_functions as ngf
 import outliers_functions as of
 
-archivo = r'C:\Users\ivan1\Desktop\Modelo-predictivo-de-errores-en-los-datos\hotel_bookings_1.csv' # se demora 2.8 seg en terminar
+#archivo = r'C:\Users\ivan1\Desktop\Modelo-predictivo-de-errores-en-los-datos\hotel_bookings_1.csv' # se demora 2.8 seg en terminar
 #archivo = r'C:\Users\ivan1\Desktop\Modelo-predictivo-de-errores-en-los-datos\hotel_bookings_1.xlsx' # se demora 43.9 seg en terminar
 
 #df = f.dataframe_from_file(archivo)
@@ -21,13 +21,15 @@ def main_primer_ingreso(df,name_file):
 	bdf.insert_unique_values_string(df_bdd_cols,df_col_string) #inserta valores unicos para columnas tipo string en la tabla UNIQUE_VALUES_STRING_COLUMNS
 
 	#--------acciones sobre columnas strings----------
-
+	print("\n\n---------------------------Corrección de nulos en columnas categóricas--------------------------------\n\n")
 	imf.imput_df_string_1ra_entrada(df,df_col_string)
+	print("\n\n----------------------Corrección de duplicados en columnas categóricas--------------------------------\n\n")
 	ngf.revisar_string_cols(df,df_col_string) #quita duplicados en las columnas string, agrega los que no se parecen a algun otro registro unico registado
 
 	#--------acciones sobre columnas numéricas--------
-
+	print("\n\n----------------------Corrección de nulos en columnas numéricas---------------------------------------\n\n")
 	imf.input_df_numerico_1ra_entrada(df,df_col_numericas)
+	print("\n\n----------------------Corrección de outliers en columnas numéricas------------------------------------\n\n")
 	of.sep_casos(df,df_col_numericas)
 
 	#Crea tabla historica y la pobla con los datos depurados del dataframe de entrada
